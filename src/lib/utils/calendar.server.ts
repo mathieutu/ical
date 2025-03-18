49aa186548eda82c50e97d4747e54cc8a721fb61
Mathieu TUDISCO <oss@mathieutu.dev>
2025-03-18 16:06:50 +0100

    ✨ Add filter and sort capacity to json api.

--- src/lib/utils/calendar.server.ts
+++ src/lib/utils/calendar.server.ts
@@ -11,3 +11,30 @@ export const fetchCalendar = async (url: string): Promise<ICAL.Component> => {
         throw new Error(`Invalid iCal data for URL: "${url}"`)
     }
 }
+
+export type Event = {
+    summary: string | undefined;
+    description: string | undefined;
+    location: string | undefined;
+    start: string | undefined;
+    end: string | undefined;
+}
+
+export type Calendar = {
+    name: string | undefined;
+    description: string | undefined;
+    timezone: string | undefined;
+    events: Event[];
+}
+export const parseCalendar = (calendar: ICAL.Component): Calendar => ({
+    name: calendar.getFirstPropertyValue("x-wr-calname")?.toString(),
+    description: calendar.getFirstPropertyValue("x-wr-caldesc")?.toString(),
+    timezone: calendar.getFirstPropertyValue("x-wr-timezone")?.toString(),
+    events: calendar.getAllSubcomponents("vevent").map(ev => ({
+        summary: ev.getFirstPropertyValue("summary")?.toString(),
+        description: ev.getFirstPropertyValue("description")?.toString(),
+        location: ev.getFirstPropertyValue("location")?.toString(),
+        start: ev.getFirstPropertyValue("dtstart")?.toString(),
+        end: ev.getFirstPropertyValue("dtend")?.toString()
+    }))
+});
--- src/routes/json/+server.ts
+++ src/routes/json/+server.ts
@@ -1,32 +1,52 @@
 import { error } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 import ICAL from "ical.js";
-import { fetchCalendar } from "$lib/utils/calendar.server";
+import { fetchCalendar, parseCalendar } from "$lib/utils/calendar.server";
 
 export const GET: RequestHandler = async ({ url }) => {
-  const icalUrl = url.searchParams.get("url");
+      const icalUrl = url.searchParams.get("url");
+      const from = url.searchParams.get("from");
+      const to = url.searchParams.get("to");
+      const summary = url.searchParams.get("summary");
+      const sort = url.searchParams.get("sort") || "date-asc";
 
-  if (!icalUrl?.length) {
-    return error(400, "No URL provided");
-  }
+      if (!icalUrl?.length) {
+        return error(400, "No URL provided");
+      }
 
-  const calendar: ICAL.Component = await fetchCalendar(icalUrl).catch((e: Error) => error(400, e.message));
-  
-  const calendarJson = {
-    name: calendar.getFirstPropertyValue('x-wr-calname'),
-    description: calendar.getFirstPropertyValue('x-wr-caldesc'),
-    timezone: calendar.getFirstPropertyValue('x-wr-timezone'),
-    events: calendar.getAllSubcomponents('vevent').map(ev => ({
-      summary: ev.getFirstPropertyValue('summary'),
-      description: ev.getFirstPropertyValue('description'),
-      location: ev.getFirstPropertyValue('location'),
-      start: ev.getFirstPropertyValue('dtstart')?.toString(),
-      end: ev.getFirstPropertyValue('dtend')?.toString()
-    }))
-  };
+      const calendar: ICAL.Component = await fetchCalendar(icalUrl).catch((e: Error) => error(400, e.message));
+      const calendarJson = parseCalendar(calendar);
 
+      const fromDate = from ? new Date(from) : null;
+      const toDate = to ? new Date(to) : null;
 
-  return new Response(JSON.stringify(calendarJson), {
-    headers: { "Content-Type": "application/json" },
-  });
-};
+  const sortedEvents = calendarJson.events
+    .filter(event => {
+        const matchesFrom = fromDate && event.start ? new Date(event.start) >= fromDate : true;
+        const matchesTo = toDate && event.end ? new Date(event.end) <= toDate : true;
+        const matchesTitle = summary && event.summary ? event.summary.includes(summary) : true;
+
+        return matchesFrom && matchesTo && matchesTitle;
+      })
+    .sort((a, b) => {
+      const [field, order] = sort.split("-");
+
+      const handleOrder = (value: number) => order === "desc" ? value * -1 : value;
+
+      if (field === "date") {
+        return handleOrder(new Date(a.start || 0).getTime() - new Date(b.start || 0).getTime())
+      }
+
+      if (field === "summary") {
+        return handleOrder((a.summary || '').localeCompare(b.summary || ''));
+      }
+
+      return 0
+    })
+
+      const filteredCalendarJson = { ...calendarJson, events: sortedEvents };
+
+      return new Response(JSON.stringify(filteredCalendarJson), {
+        headers: { "Content-Type": "application/json" },
+      });
+    };
